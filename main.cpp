7c956dc296cbf10bc05604b1c75d16729a4a20d9
Alice (RozumenkoVM) <alice@example.com>
2024-07-01 09:59:31 +0300

    code: исправление ошибок

--- main.cpp
+++ main.cpp
@@ -7,15 +7,22 @@ int main()
     cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
-int max ;
+int min;
+if (a < b) {
+    min = a;
+} else {
+min = b;}
+int max;
 if (a > b) {
     max = a;
 } else {
 max = b;}
+
 cout << "A + B = " << a + b << '\n'
 << "A - B = " << a - b << '\n'
 << "A * B = " << a * b << '\n'
 << "A / B = " << a / b << '\n'
-<< "MAX:" << max << '\n';
+<< "MIN = " << min << '\n'
+<< "MAX = " << max << '\n';
     return 0;
 }
