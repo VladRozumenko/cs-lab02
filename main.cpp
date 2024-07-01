7cdeb8a4b4d466fc4911deb22fae0dd04703024d
Alice (RozumenkoVM) <alice@example.com>
2024-07-01 09:20:40 +0300

    code: добавление вывода максимума функции

--- main.cpp
+++ main.cpp
@@ -7,9 +7,15 @@ int main()
     cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
+int max ;
+if (a > b) {
+    max = a;
+} else {
+max = b;}
 cout << "A + B = " << a + b << '\n'
 << "A - B = " << a - b << '\n'
 << "A * B = " << a * b << '\n'
-<< "A / B = " << a / b << '\n';
+<< "A / B = " << a / b << '\n'
+<< "MAX:" << max << '\n';
     return 0;
 }
